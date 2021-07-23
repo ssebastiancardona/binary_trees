1888d7fdd1490a084e2cea7bba0c29233bd66e48
ssebastiancardona <ssebastiancardona@gmail.com>
2021-07-22 20:39:33 -0500

    0-binary_tree_node.c

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_node - crea un nodo de árbol binario
+ * @parent: parte del nodo a crear
+ * @value: valor para almacenar en el nuevo nodo
+ *
+ * Retorno: puntero al nuevo nodo
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->left = new_node->right = NULL;
+	return (new_node);
+}
